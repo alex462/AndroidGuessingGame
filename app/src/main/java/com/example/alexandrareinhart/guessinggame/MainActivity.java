c3b39a9a99eb368a17ad210b7cae109a627d6c32
alex462 <alex.reinhart462@gmail.com>
2018-03-13 16:30:11 -0400

    Created GameActivity and finished its XML. Started logic of app in mainActivity.

--- app/src/main/java/com/example/alexandrareinhart/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/alexandrareinhart/guessinggame/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.alexandrareinhart.guessinggame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) { //every class that is accessed by a user has an onCreate
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        startGame();
+    }
+
+    private void startGame(){
+        Intent playGame = new Intent(this, GameActivity.class); //this class needs to change from what it is to the game class. it still
+        //doesn't know what it needs to do until I tell it what my intent to do is.
+        startActivity(playGame);
     }
 }
