6cadf3142c61996d3c796ff978136a420d2e476a
alex462 <alex.reinhart462@gmail.com>
2018-03-14 15:33:25 -0400

    working on methods in mainActivity

--- app/src/main/java/com/example/alexandrareinhart/guessinggame/GameActivity.java
+++ app/src/main/java/com/example/alexandrareinhart/guessinggame/GameActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
 
+    //private TextView guessHint;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //guessHint = findViewByID(R.id.clue_textview); //doesn't need a listener. is built into screen layout
     }
+
+    /*
+    private
+     */
 }
--- app/src/main/java/com/example/alexandrareinhart/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/alexandrareinhart/guessinggame/MainActivity.java
@@ -3,15 +3,41 @@ package com.example.alexandrareinhart.guessinggame;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.method.KeyListener;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button startButton;
+    private TextView directions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) { //every class that is accessed by a user has an onCreate
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState); //these 2 lines should ALWAYS be the first 2 lines ever.
+        setContentView(R.layout.activity_main); //NEVER mess with these 2 lines
 
-//        startGame();
+        startButton = findViewById(R.id.start_button); //R stands for resource.
+        directions = findViewById(R.id.directions_textview);
+
+        startListener();
+        //set a listener. at all times, device needs to be listening for input, i.e. tap, touch, tilt, etc. must set listener/s AS SOON AS app has started.
+
+    }
+
+    private void startListener(){
+
+        startButton.setOnClickListener(new View.OnClickListener() { //OnClickListener is a class built in to Android
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+
+        //directions.SOMETHING
     }
 
     private void startGame(){
