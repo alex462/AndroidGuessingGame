e4b7bba0493d9ffabd19b3d8c24d529c6c93a421
alex462 <alex.reinhart462@gmail.com>
2018-03-16 16:32:04 -0400

    set up layout in resultsActivity and began code in .java

--- app/src/main/java/com/example/alexandrareinhart/guessinggame/ResultsActivity.java
+++ app/src/main/java/com/example/alexandrareinhart/guessinggame/ResultsActivity.java
@@ -2,12 +2,39 @@ package com.example.alexandrareinhart.guessinggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    private Button playAgainButton;
+    private TextView correctNumber;
+    private TextView winOrLoseResultTextview;
+    private ImageView winOrLoseResultImageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        playAgainButton = findViewById(R.id.play_again_button);
+        correctNumber = findViewById(R.id.generated_number_textview);
+        winOrLoseResultTextview = findViewById(R.id.results_output_textview);
+        winOrLoseResultImageview = findViewById(R.id.winner_imageview);
+
+        setListener();
     }
+
+    private void setListener() {
+        playAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
 }
